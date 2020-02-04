52d445c739ff0cfb9b06acce532d6a5d1b54f27f
Alyssa Huberts <alyssa_huberts@g.harvard.edu>
2020-02-04 11:37:33 -0500

    Problem set 1- to be done in class

---
+++ ps_1_assignment.R
@@ -0,0 +1,5 @@
+This is the README for problem set 1. 
+In class, Preceptor will walk you through the process 
+of creating a new file, ps_1.Rmd, knitting it into ps_1.html, 
+committing/pushing both to the GitHub repo (which is personal to you) 
+which was created for this assignment by TF Alyssa using GitHub Classroom.
